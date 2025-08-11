0e109bcd28ebd2c862bf900b35e44a6254906f5c
jhin93 <scene1993@gmail.com>
2025-08-11 22:31:42 +1000

    chagning

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! This is testing");
+            Console.WriteLine("Enter the numbers:");
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out int number))
+            {
+                int test = SumAverageLargestSmallest(number);
+                Console.WriteLine(test);
+            }
+            else
+            {
+                Console.WriteLine("Enter only numbers.");
+            }
         }
 
         static int SumAverageLargestSmallest(int input)
